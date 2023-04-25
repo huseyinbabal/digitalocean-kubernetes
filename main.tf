b704b11b8c0d253fb70b61fdd88d9aa14ab90be0
Huseyin BABAL <huseyinbabal88@gmail.com>
2023-04-25 14:24:11 +0300

    helm release added

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "helm" {
+  kubernetes {
+    config_path = digitalocean_kubernetes_cluster.keptn.kube_config
+  }  
+}
+
 resource "digitalocean_kubernetes_cluster" "keptn" {
   name   = "keptn"
   region = "nyc1"
@@ -9,4 +15,12 @@ resource "digitalocean_kubernetes_cluster" "keptn" {
     size       = "s-4vcpu-8gb"
     node_count = 2
   }
+}
+
+resource "helm_release" "keptn" {
+  repository = "https://charts.keptn.sh"
+  chart = "keptn"
+  name  = "keptn"
+
+  depends_on = [digitalocean_kubernetes_cluster.keptn]
 }
--- versions.tf
+++ versions.tf
@@ -4,5 +4,15 @@ terraform {
       source = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
+
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.9.0"
+    }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.20.0"
+    }
   }
 }
